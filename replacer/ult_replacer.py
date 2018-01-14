1869e2949e8984e06ad30349c6d9cd3c46e8486a
Eric Davis <edavis@quadwtech.com>
2018-01-14 00:20:42 -0600

    deleted unnecessary code, added while loop for correct file name resubmit added comments

--- replacer/ult_replacer.py
+++ replacer/ult_replacer.py
@@ -4,28 +4,36 @@ import os
 import glob
 import sys
 
+# Checks if the file exists in the folder
 def file_check(file_name):
 	try:
 		file_name = raw_input("File name to search?\n"
 			"Please inclue file type\n\n")
 		reading = open(file_name)
 		reading.close()
-		return(file_name)
+		return file_name
 
 	except IOError:
 		print('Please enter an existing file.\n\n')
-		sys.exit()
+		file_name = ''
+		return file_name
 
 
 file_name = ''
-file_name = file_check(file_name)
+
+# loop allowing for resubmit of file name
+while file_name == '':
+	file_name = file_check(file_name)
+
+
 reading = open(file_name)
+
+# pulls text from file and stores in a variable
 text = reading.read()
 reading.close()
 print text
 
 ######################################################
-
 
 def the_gen(text):
 
@@ -38,9 +46,8 @@ def the_gen(text):
 			'to make this change?\n\n')
 		num = input('How many times per round?\n\n')
 
-	
-######################################################
-
+		
+		# loop for replacement cycles
 		for x in range(times):
 			x = x + 1
 			new_item = raw_input('What would you like to replace '
@@ -64,28 +71,15 @@ def the_gen(text):
 
 	text = editing.write(text)
 
-	editing.close
+	editing.close()
 
+	# displays new text with replaced content
 	editing = open(os.path.join(file_name), 'r')
 	new_text = editing.read()
 	editing.close()
 	print new_text
 	sys.exit()
 
-#######################################################
-
-#	answer = raw_input('Would you like to replace in the same '
-#		'file?\n yes\n no\n\n')
-
-#	if answer == 'yes':
-#		the_gen(text)
-#	if answer == 'no':
-#		return(text)
-#		sys.exit()
-#	else:
-#		print ('Please pick one of the options.\n\n')
-
-#	return(text)
 
 
 
