f0fb9597b90c69107d284d3fd6999d46fe3cd693
Eric Davis <edavis@quadwtech.com>
2018-05-23 18:43:29 -0500

    revamp search tool
    displays in the remains file where the search term was found

--- search/ult_search.py
+++ search/ult_search.py
@@ -1,76 +1,116 @@
-# ult_search mass searches through content and returns a results sheet.
+# Searches queries in a batch of content and displays line of content's first occurance
 
 import io
 import os
-import glob
 import sys
 
-# Opens file with the content you would like to search
-reading = open("../ZZ_data_ZZ/content.txt")
-text = reading.read()
-reading.close()
-
-######################################################
-
-
-def the_gen(text):
-	# Opens the file where the results will be stored
-	results = open(os.path.join("../ZZ_data_ZZ/results.txt"), 'w')
-
+def counter(count):
 	try:
-		times = input('How many items would you like '
-			'to search?\n\n')
-
-		# Make the text lowercase to find all variations of search
-		text = text.lower()
-######################################################
-
-		for x in range(times):
-			x = x + 1
-
-			# allows input for the word or phrase you would like to search
-			search = raw_input('What are you searching? '
-				+ str(x) + '\n\n')
-			# Make the text lowercase to find all variations of search
-			search = search.lower()
-
-			# Finds and counts the desired content
-			finding = text.find(search)
-			counting = text.count(search)
-			counting = str(counting)
-
-			# for reporting on findings in the results file
-			if finding == -1:
-				results.write("\n\nCouldn't find //// " + search)
-			else:
-				results.write("\n\nFound //// " + search + " ---- " + counting)
-
-			
-	except ValueError:
-		print('\n\n\n\nPlease enter a number.\n\n\n\n')
-		the_gen(text)
+		count = input('How many phrases would you like to search?\n\n')
+		return count
 	except SyntaxError:
-		print('\n\n\n\nPlease enter a number.\n\n\n\n')
-		the_gen(text)
-	except TypeError:
-		print('\n\n\n\nPlease enter a whole number.\n\n\n\n')
-		the_gen(text)
+		print 'Please use a number.\n\n'
+		return
 	except NameError:
-		print('\n\n\n\nPlease enter a whole number.\n\n\n\n')
-		the_gen(text)
+		print 'Please use a number.\n\n'
+		return
 
+def the_gen():
+
+	count = None
+
+	# to check input for an actual number
+	while count < 0:
+		count = counter(count)
 	
+	# Opens file content you would like to search and creates a list out of the file lines
+	reading = open("../ZZ_data_ZZ/content.txt")
+	lines = reading.readlines()
+	reading.close()
 
-	results.close()
+	# open file for writing
+	pen = open("../ZZ_data_ZZ/results.txt", 'w')
 
-	# For displaying the search results after the code has finished
-	r_results = open(os.path.join("../ZZ_data_ZZ/results.txt"), 'r')
-	new_text = r_results.read()
-	r_results.close()
+	term_list = []
 
+	# Adds terms to list
+	for x in range(count):
+		term_list += [raw_input('What would you like to search for? '
+				+ str(x + 1) + '\n\n')]
+
+	content_lines_found = []
+	to_remains = []
+
+	num = 0
+
+	# searchs terms by each content's line
+	for x in term_list:
+
+		num = num + 1
+
+		# makes term lowercase
+		term = x.lower()
+
+		# search for term in content lines
+		for y in lines:
+
+			line = y.lower()
+			finding = line.find(term)
+			counting = line.count(term)
+
+			# conditions for finding term
+			if finding != -1:
+				no_luck = False
+				found_here = y
+				break
+
+			if finding == -1:
+				no_luck = True
+				found_here = "****\n"
+
+		# lines with unfound items will be written
+		if no_luck == True:
+			pen.write("\n\nCouldn't find //// " + x)
+			to_remains = [found_here]
+
+		# Found items will be saved to be written to remains
+		if no_luck == False:
+			pen.write("\n\nFound //// " + str(x) + " ---- " + str(counting))
+			to_remains = [found_here]
+		
+		content_lines_found += to_remains
+
+	pen.close()
+
+
+	# Writing found location to remains.txt.
+	# good lord please check your content!
+	remains = open("../ZZ_data_ZZ/remains.txt", 'w')
+	num = 0
+
+
+	for x in content_lines_found:
+		num = num + 1
+		remains.write(x)
+		print "Writing remains - " + str(num) + " / " + str(len(content_lines_found))
+
+	remains.close()
 	sys.exit()
 
+the_gen()
 
-the_gen(text)
+print "\nOpen the results.txt file and search 'Found' or 'Couldn't find' to quickly check "
+"results\n\nAnd open remains.txt to see the line of the first instance the content was found\n"
 
-print "\nOpen the results.txt file and search 'Found' or 'Couldn't find' to quickly check results\n"
+
+
+
+
+
+
+
+
+
+
+
+
